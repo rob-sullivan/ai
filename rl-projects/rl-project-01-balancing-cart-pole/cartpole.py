3515a7866cee40f737aeb6853d62579739250c9f
Rob Sullivan <robertosullivan5@gmail.com>
2022-12-29 21:57:50 +0000

    cartpole

--- rl-projects/rl-project-01-balancing-cart-pole/cartpole.py
+++ rl-projects/rl-project-01-balancing-cart-pole/cartpole.py
@@ -5,4 +5,19 @@ import random
 env = gym.make('CartPole-v0')
 
 states = env.observation_space.shape[0]
-actions = env.action_space.shape[0]
+actions = env.action_space.n
+
+#print(str(actions))
+
+episodes = 10
+for episode in range(1, episodes+1):
+    state = env.reset()
+    done = False
+    score = 0
+
+    while not done:
+        env.render()
+        action = random.choice([0,1])
+        n_state, reward, done, truncated, info = env.step(action)
+        score += reward
+    print('Episode:{} Score: {}'.format(episode, score))
