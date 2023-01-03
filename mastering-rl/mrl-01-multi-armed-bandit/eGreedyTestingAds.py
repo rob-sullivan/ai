87fb865f3ebd87e3f75d4c172398a80c09d5ce7f
Rob Sullivan <robertosullivan5@gmail.com>
2023-01-03 02:10:21 +0000

    epg

--- mastering-rl/mrl-01-multi-armed-bandit/eGreedyTestingAds.py
+++ mastering-rl/mrl-01-multi-armed-bandit/eGreedyTestingAds.py
@@ -2,6 +2,12 @@ import numpy as np
 import pandas as pd
 import cufflinks as cf
 import plotly.offline
+cf.go_offline() # required to use plotly offline (no account required).
+cf.set_config_file(world_readable=True, theme="white")
+import plotly.io as pio
+pio.renderers.default = "browser"
+import plotly.graph_objects as go
+pd.options.plotting.backend = "plotly"
 
 class Advert():
     """
@@ -14,6 +20,46 @@ class Advert():
         reward = np.random.binomial(n=1, p=self.p)
         return reward
 
+class epsilonGreedy():
+    def __init__(self, a):
+        self.ads = a
+        self.dfRewardComparision = pd.DataFrame([])
+
+        #run tests, start with a high ε value and gradually reduce it
+        self.choose(0.2)
+        self.choose(0.1)
+        self.choose(0.05)
+        self.choose(0.01)
+
+        greedy_list = ['e-greedy: 0.01', 'e-greedy: 0.05','e-greedy: 0.1', 'e-greedy: 0.2']
+        print(self.dfRewardComparision.head())
+        self.dfRewardComparision[greedy_list].iplot(title="ε-Greedy Actions", dash=['solid', 'dash', 'dashdot', 'dot'], xTitle='Impressions', yTitle='Avg. Reward').show(renderer="png")
+
+    #epsilon greedy
+    def choose(self, eps):
+        nProd = 100000
+        nAds = len(self.ads)
+        Q = np.zeros(nAds) # action values
+        N = np.zeros(nAds) # total impressions
+        totalReward = 0
+        avgRewards = [] #save average reward over time
+
+        adChosen = np.random.randint(nAds)
+        for i in range(nProd):
+            R = self.ads[adChosen].displayAd()
+            N[adChosen] += 1
+            Q[adChosen] += (1 / N[adChosen]) * (R - Q[adChosen])
+            totalReward += R
+            avgReward = totalReward / (i + 1)
+            avgRewards.append(avgReward)
+
+            # Select the next ad to display
+            if np.random.uniform() <= eps:
+                adChosen = np.random.randint(nAds)
+            else:
+                adChosen = np.argmax(Q)
+
+        self.dfRewardComparision['e-greedy: {}'.format(eps)] = avgRewards
 
 if __name__ == "__main__":
     adA = Advert(0.004)
@@ -24,27 +70,4 @@ if __name__ == "__main__":
 
     ads = [adA, adB, adC, adD, adE]
 
-    #epsilon greedy
-    eps = 0.1 #start with a high ε value and gradually reduce it
-    nProd = 100000
-    nAds = len(ads)
-    Q = np.zeros(nAds) # action values
-    N = np.zeros(nAds) # total impressions
-    totalReward = 0
-    avgRewards = [] #save average reward over time
-
-    adChosen = np.random.randint(nAds)
-    for i in range(nProd):
-        R = ads[adChosen].display_ad()
-        N[adChosen] += 1
-        Q[adChosen] += (1 / N[adChosen]) * (R - Q[adChosen])
-        totalReward += R
-        avgReward = totalReward / (i + 1)
-        avgRewards.append(avgReward)
-
-    # Select the next ad to display
-    if np.random.uniform() <= eps:
-        adChosen = np.random.randint(nAds)
-    else:
-        adChosen = np.argmax(Q)
-    dfRewardComparision['e-greedy: {}'.format(eps)] = avgRewards
+    epsilonGreedy(ads)
